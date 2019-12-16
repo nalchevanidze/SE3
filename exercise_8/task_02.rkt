f0bcb24d5569c313a3ac8d979f72b7a36e26a790
nalchevanidze <d.nalchevanidze@gmail.com>
2019-12-16 02:06:12 +0100

    update

---
+++ exercise_8/task_02.rkt
@@ -0,0 +1,10 @@
+;1. Geben Sie einen Ausdruck an, der die Liste der Quadrierungen (x2) aller Zahlen der Liste xs berechnet.
+
+
+;2. Geben Sie einen Ausdruck an, der die Teilliste aller glatt durch 3 oder 7 teilbaren Zahlen von xs konstruiert.
+
+
+;3. Geben Sie einen Ausdruck an, der die Summe der geraden Zahlen grö- ÿer 6 in xs ermittelt.
+
+
+;4. Zusatzaufgabe: Geben Sie einen Ausdruck an, welcher eine Liste anhand eines Prädikats (z.B. odd?) in zwei Teillisten aufspaltet und zurückgibt.
