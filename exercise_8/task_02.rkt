18d7ca70bc87dca58f2c49ded47c18953d270e48
nalchevanidze <d.nalchevanidze@gmail.com>
2019-12-16 02:28:10 +0100

    update 2

--- exercise_8/task_02.rkt
+++ exercise_8/task_02.rkt
@@ -1,10 +1,44 @@
+#lang racket
+
 ;1. Geben Sie einen Ausdruck an, der die Liste der Quadrierungen (x2) aller Zahlen der Liste xs berechnet.
 
 
+
+(define xs->sqr 
+    (curry map sqr)
+)
+
+(xs->sqr (range 10))
+
 ;2. Geben Sie einen Ausdruck an, der die Teilliste aller glatt durch 3 oder 7 teilbaren Zahlen von xs konstruiert.
 
+(define (tailbar? x)
+    (and 
+        (< 0 x) 
+        (ormap (compose 
+            (curry eq? 0) 
+            (curry modulo x) 
+        ) 
+        (list 7 3))
+    )
+)
 
-;3. Geben Sie einen Ausdruck an, der die Summe der geraden Zahlen grö- ÿer 6 in xs ermittelt.
+(define tailbar 
+    (curry filter tailbar?)
+)
+
+(tailbar (range 10))
+
+
+;3. Geben Sie einen Ausdruck an, der die Summe der geraden Zahlen größer 6 in xs ermittelt.
 
 
 ;4. Zusatzaufgabe: Geben Sie einen Ausdruck an, welcher eine Liste anhand eines Prädikats (z.B. odd?) in zwei Teillisten aufspaltet und zurückgibt.
+
+(define (my-partition f xs) 
+    (list (filter f xs) (filter (compose not f) xs))
+) 
+
+(my-partition even? (range 10))
+;; or 
+;; (partition even? (range 10))
